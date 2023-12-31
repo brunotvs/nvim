1449d0e0be87f1c9f0dc6e4b86346de905f617c1
Bruno Thiago <brunothiagorvs@gmail.com>
2023-12-31 18:10:16 -0300

    feat: rust neotest

--- lua/plugins/languages/rust.lua
+++ lua/plugins/languages/rust.lua
@@ -2,4 +2,14 @@ LspServers['rust_analyzer'] = {}
 table.insert(TreesitterEnsureInstalled, 'rust')
 return {
   { 'williamboman/mason-lspconfig.nvim' },
+  {
+    'rouge8/neotest-rust',
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-rust")
+        }
+      })
+    end
+  }
 }
