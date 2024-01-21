be81a66fd1b4ea7bb954c8e5e5cb269251319f87
Bruno Thiago <brunothiagorvs@gmail.com>
2024-01-21 02:50:11 -0300

    fix(harpoon): updated select one or multi function for harpoon2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,7 +4,11 @@ local select_one_or_multi = function(prompt_bufnr)
   if not vim.tbl_isempty(multi) then
     for _, j in pairs(multi) do
       if j.path ~= nil then
-        require("harpoon.mark").add_file(j.path)
+        local harpoon = require("harpoon")
+        local Path = require("plenary.path")
+        local cwd = vim.loop.cwd()
+        local item_path = Path:new(j.path):make_relative(cwd)
+        harpoon:list():append({ value = item_path, context = { row = 1, col = 0 } })
       end
     end
   end
@@ -40,6 +44,6 @@ return {
     { "<leader>hj", function() require("harpoon"):list():select(2) end,                                desc = "Harpoon: Nav file 2" },
     { "<leader>hk", function() require("harpoon"):list():select(3) end,                                desc = "Harpoon: Nav file 3" },
     { "<leader>hl", function() require("harpoon"):list():select(4) end,                                desc = "Harpoon: Nav file 4" },
-    { "<leader>hc", function() require("harpoon.mark").clear_all() end,                                desc = "Harpoon: Clear all" }
+    { "<leader>hc", function() require("harpoon"):list():clear() end,                                  desc = "Harpoon: Clear all" }
   }
 }
