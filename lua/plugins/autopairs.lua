890ef56f5569123b3d7ad83da22fcfc5b7247377
Bruno Thiago <brunothiagorvs@gmail.com>
2024-01-22 17:37:01 -0300

    chore: deleted unused plugins

--- lua/plugins/autopairs.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'windwp/nvim-autopairs',
-  enabled = false,
-  dependencies = { 'hrsh7th/nvim-cmp' },
-  event = "InsertEnter",
-  opts = {}, -- this is equalent to setup({}) function
-}
--- lua/plugins/coc.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'neoclide/coc.nvim',
-  branch = 'release',
-  config = function()
-    vim.g.coc_global_extensions = { 'coc-git', 'coc-json', 'coc-pyright' }
-  end,
-  enabled = false
-}
