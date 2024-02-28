508f992d0f3c8d85a3c2a016bf6870c761ec0cf5
Bruno Thiago <brunothiagorvs@gmail.com>
2024-02-28 01:20:06 -0300

    feat(undotree): focus on toggle

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,6 +1,14 @@
 return {
   'mbbill/undotree',
+  config = function()
+    --  if set, let undotree window get focus after being opened, otherwise
+    --  focus will stay in current window.
+    if vim.g.undotree_SetFocusWhenToggle then
+      vim.g.undotree_SetFocusWhenToggle = 1
+    end
+  end,
   keys = {
+
     { '<M-u>', vim.cmd.UndotreeToggle, desc = 'UndoTree: Toggle', mode = { 'n', 'v' } },
   },
 }
