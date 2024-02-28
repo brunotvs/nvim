d2c4b8ac0baeed2962c33193cfcce9c6fa658e40
Bruno Thiago <brunothiagorvs@gmail.com>
2024-02-28 01:19:49 -0300

    fix(mason-lspconfig): seting up capabilities

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -87,7 +87,7 @@ return {
 
       mason_lspconfig.setup_handlers({
         function(server_name)
-          local opts = { on_attach = on_attach }
+          local opts = { on_attach = on_attach, capabilities = capabilities }
           for key, value in pairs(LspServers[server_name]) do
             if key ~= 'on_attach' then
               opts[key] = value
