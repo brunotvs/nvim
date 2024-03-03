dd27635e579a8013ad0c6318c737101bfd58b278
Bruno Thiago <brunothiagorvs@gmail.com>
2024-03-02 23:04:50 -0300

    feat(conform): commands for enabling and disabling autoformat on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,8 +1,8 @@
 local slow_format_filetypes = {}
 return {
   'stevearc/conform.nvim',
-  dev = true,
-  url = 'https://github.com/brunotvs/conform.nvim.git',
+  dev = false,
+  -- url = 'https://github.com/brunotvs/conform.nvim.git',
   optional = true,
   event = { 'BufReadPre', 'BufNewFile' },
   opts = {
@@ -39,5 +39,22 @@ return {
       require('conform').format({ async = true, lsp_fallback = true, range = range, filter = FilterServers })
     end, { range = true })
     vim.keymap.set('n', '<leader>f', '<cmd>Format<CR>', { desc = 'Conform: Format current buffer' })
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true,
+    })
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Re-enable autoformat-on-save',
+    })
   end,
 }
