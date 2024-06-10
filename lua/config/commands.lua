775d05893824a40cae7502081416cbd42869cb50
Bruno Thiago <brunothiagorvs@gmail.com>
2024-06-10 17:44:28 -0300

    fix: updated deprecated code

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_user_command('LspStopAllClients', function()
-  local clients = vim.lsp.get_active_clients()
+  local clients = vim.lsp.get_clients()
 
   local plural = #clients == 1 and '' or 's'
   vim.notify('Stopping ' .. #clients .. ' client' .. plural)
