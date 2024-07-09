8f9b00cb80d4a24907f22a718a1991cacfc9a53d
Bruno Thiago <brunothiagorvs@gmail.com>
2024-07-09 16:49:25 -0300

    chore: clear wrappign configs

--- lua/plugins/wrapping.lua
+++ lua/plugins/wrapping.lua
@@ -2,18 +2,6 @@
 return {
   'andrewferrier/wrapping.nvim',
   lazy = false,
-  opts = {
-    auto_set_mode_filetype_allowlist = {
-      'asciidoc',
-      'gitcommit',
-      'latex',
-      'mail',
-      'markdown',
-      'rst',
-      'tex',
-      'text',
-    },
-  },
   config = function(_, opts)
     require('wrapping').setup(opts)
   end,
