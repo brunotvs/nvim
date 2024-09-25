2c0c82aee996dea3ff3f2a650dbf15229385fc27
Bruno Thiago <brunothiagorvs@gmail.com>
2024-09-25 17:49:52 -0300

    feat: removed mappings from nvim-ipy

--- lua/plugins/languages/python.lua
+++ lua/plugins/languages/python.lua
@@ -26,5 +26,10 @@ return {
       'nvim-neotest/neotest',
     },
   },
-  { 'bfredl/nvim-ipy' },
+  {
+    'bfredl/nvim-ipy',
+    init = function()
+      vim.g.nvim_ipy_perform_mappings = 0
+    end,
+  },
 }
