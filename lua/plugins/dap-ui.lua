29a3828e1130018c2ef79fe4209efd2bb490a617
Bruno Thiago <brunothiagorvs@gmail.com>
2025-01-03 19:30:28 -0300

    feat(dap-ui): using default layouts

--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -1,75 +1,4 @@
 ---@module "dapui"
---- @type table<string,dapui.Config.layout[]>
-local named_layouts = {
-  Default = {
-    {
-      elements = {
-        {
-          id = 'scopes',
-          size = 0.25,
-        },
-        {
-          id = 'breakpoints',
-          size = 0.25,
-        },
-        {
-          id = 'stacks',
-          size = 0.25,
-        },
-        {
-          id = 'watches',
-          size = 0.25,
-        },
-      },
-      position = 'left',
-      size = 40,
-    },
-    {
-      elements = { {
-        id = 'repl',
-        size = 0.5,
-      }, {
-        id = 'console',
-        size = 0.5,
-      } },
-      position = 'bottom',
-      size = 10,
-    },
-  },
-  Other = {
-    elements = {
-      {
-        id = 'scopes',
-        size = 0.25,
-      },
-      {
-        id = 'breakpoints',
-        size = 0.25,
-      },
-      {
-        id = 'stacks',
-        size = 0.25,
-      },
-      {
-        id = 'watches',
-        size = 0.25,
-      },
-    },
-    position = 'left',
-    size = 40,
-  },
-}
-
-local additional_layouts = {
-  'Other',
-}
-
-local layouts = {}
-table.insert(layouts, named_layouts.Default)
-
-for _, value in ipairs(additional_layouts) do
-  table.insert(layouts, named_layouts[value])
-end
 
 --- @type LazySpec
 return {
@@ -80,24 +9,13 @@ return {
   },
   main = 'dapui',
   ---@diagnostic disable: missing-fields
-  --- @type dapui.Config?
-  opts = {
-    layout = layouts,
-  },
+  --- @type dapui.Config
+  opts = {},
   ---@diagnostic enable: missing-fields
   init = function()
     local dap = require('dap')
     local dapui = require('dapui')
-    dap.listeners.after.event_initialized['dapui_config'] = function()
-      local input_list = { 'Choose layout: ', '1. Default' }
-      for index, value in ipairs(additional_layouts) do
-        local input = index + 1 .. '. ' .. value
-        table.insert(input_list, input)
-      end
-      local layout = vim.fn.inputlist(input_list)
-      layout = math.max(layout, 1)
-      dapui.open({ layout = layout })
-    end
+    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
   end,
