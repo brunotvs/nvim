cff3db4d413145d0470cd7bbfe750595d0eb53e3
Bruno Thiago <brunothiagorvs@gmail.com>
2025-01-08 20:17:40 -0300

    feat: conform disable format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -8,6 +8,9 @@ return {
   event = { 'BufReadPre', 'BufNewFile' },
   opts = {
     format_on_save = function(bufnr)
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
       if slow_format_filetypes[vim.bo[bufnr].filetype] then
         return
       end
@@ -21,6 +24,9 @@ return {
     end,
 
     format_after_save = function(bufnr)
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
       if not slow_format_filetypes[vim.bo[bufnr].filetype] then
         return
       end
