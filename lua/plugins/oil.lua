cada682bc2f4cb9ee2c6c4e715214c875ee8f55f
Bruno Thiago <brunothiagorvs@gmail.com>
2025-01-27 18:34:27 -0300

    feat: oil always hidden

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,6 +6,9 @@ return {
   opts = {
     view_options = {
       show_hidden = true,
+      is_always_hidden = function(name, _)
+        return name == '..' or name == '.git'
+      end,
     },
   },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
