fc8d5b9cf678edb4025444ab89b43ba215e29fd3
Bruno Thiago <brunothiagorvs@gmail.com>
2025-03-07 09:38:04 -0300

    chore: harpoon telescope keymap

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -72,7 +72,7 @@ return {
       desc = 'Harpoon: Toggle menu',
     },
     {
-      '<leader>hf',
+      '<leader>hs',
       function()
         toggle_telescope(require('harpoon'):list())
       end,
