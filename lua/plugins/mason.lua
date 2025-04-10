1abf3d0ebfe79d2598138350b09e64530e37fd6c
Bruno Thiago <brunothiagorvs@gmail.com>
2025-04-09 21:05:22 -0300

    refactor: extracted function

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,3 +1,13 @@
+local function ensure_installed(packages)
+  local mr = require('mason-registry')
+  for _, tool in ipairs(packages) do
+    local p = mr.get_package(tool)
+    if not p:is_installed() then
+      p:install()
+    end
+  end
+end
+
 --- @module "mason"
 --- @type LazySpec
 return {
@@ -15,18 +25,12 @@ return {
   config = function(_, opts)
     require('mason').setup(opts)
     local mr = require('mason-registry')
-    local function ensure_installed()
-      for _, tool in ipairs(opts.ensure_installed) do
-        local p = mr.get_package(tool)
-        if not p:is_installed() then
-          p:install()
-        end
-      end
-    end
     if mr.refresh then
-      mr.refresh(ensure_installed)
+      mr.refresh(function()
+        ensure_installed(opts.ensure_installed)
+      end)
     else
-      ensure_installed()
+      ensure_installed(opts.ensure_installed)
     end
   end,
   keys = { { '<M-m>', '<cmd>Mason<CR>', desc = 'Mason: Open' } },
