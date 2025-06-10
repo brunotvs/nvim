17e054a0a156c1a3214612e6c259d8e92911fab4
Bruno Thiago <brunothiagorvs@gmail.com>
2025-06-10 20:00:57 -0300

    feat: golang dap

--- lua/plugins/languages/go.lua
+++ lua/plugins/languages/go.lua
@@ -2,12 +2,18 @@ TableInsert(LspServers, {
   gopls = {},
 })
 
-TableInsert(MasonEnsureInstalled, { 'go-debug-adapter' })
+TableInsert(MasonEnsureInstalled, { 'go-debug-adapter', 'delve' })
 TableInsert(TreesitterEnsureInstalled, { 'go' })
 TableInsert(NeotestAdapters, { ['neotest-golang'] = {} })
 
 ---@type LazySpec
 return {
-  'fredrikaverpil/neotest-golang',
-  dependencies = { 'neotest/neotest' },
+  {
+    'fredrikaverpil/neotest-golang',
+    dependencies = { 'neotest/neotest' },
+  },
+  {
+    'leoluz/nvim-dap-go',
+    opts = {},
+  },
 }
