6fe45a1257c3444b5f297c5cd8853f5b27ad8262
Bruno Thiago <brunothiagorvs@gmail.com>
2026-03-16 20:40:40 -0300

    feat: using nmake if win32

--- lua/plugins/telescope-fzf-native.lua
+++ lua/plugins/telescope-fzf-native.lua
@@ -1,7 +1,7 @@
 --- @type LazySpec
 return {
   'nvim-telescope/telescope-fzf-native.nvim',
-  build = 'make',
+  build = vim.fn.has('win32') == 1 and 'nmake' or 'make',
   dependencies = { 'nvim-telescope/telescope.nvim' },
   init = function()
     require('telescope').load_extension('fzf')
