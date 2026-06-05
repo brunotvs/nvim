39afae462fa63000c3315f4270e3ef055bf17496
Bruno Thiago <brunothiagorvs@gmail.com>
2026-06-05 20:14:32 -0300

    feat: turtle config

---
+++ lua/plugins/languages/turtle.lua
@@ -0,0 +1,19 @@
+LspServers = vim.tbl_extend('force', LspServers, {
+  turtle_ls = {},
+})
+
+--- @type LazySpec
+return {
+  'williamboman/mason-lspconfig.nvim',
+  dependencies = {
+    'turtle-config',
+    virtual = true,
+    config = function()
+      local mappings_server = require('mason-lspconfig.mappings.server')
+      mappings_server.lspconfig_to_package['turtle_ls'] = 'turtle-language-server'
+      mappings_server.package_to_lspconfig['turtle-language-server'] = 'turtle_ls'
+      local mappings_filetypes = require('mason-lspconfig.mappings.filetype')
+      mappings_filetypes.turtle = { 'turtle_ls' }
+    end,
+  },
+}
